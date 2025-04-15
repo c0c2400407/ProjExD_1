079b43f44f53a3d221df8301031a6c4f0a8a95af
c0c2400407 <c0c2400407@edu.teu.ac.jp>
2025-04-15 18:28:06 +0900

    演習1-①：背景と同じスピード

--- pygame_image.py
+++ pygame_image.py
@@ -12,41 +12,51 @@ def main():
     bg_img = pg.image.load("fig/pg_bg.jpg")
     bg_img2 = pg.transform.flip(bg_img, True, False)
     kk_img = pg.image.load("fig/3.png")
+    kk_img = pg.transform.flip(kk_img, True, False)
+    kk_rct=kk_img.get_rect()
+    kk_rct.center=300, 200
     #enn=pg.Surface((20,20,))
     #pg.draw.circle(enn,(255,0,0),(10,10),10)
 
 
-    kk_img=pg.transform.flip(kk_img, True, False)
-    kk_rct=kk_img.get_rect()
-    kk_rct.center=300, 200
+
 
     tmr = 0
+    p=0
+    q=0
     while True:
         for event in pg.event.get():
-            if event.type == pg.QUIT: return
+           if event.type == pg.QUIT: return
         key_lst=pg.key.get_pressed()
-        #print(key_lst[pg.k_up])
+            #print(key_lst[pg.k_up]
         if key_lst[pg.K_UP]:
-            kk_rct.move_ip(0, -1)
-        if key_lst[pg.K_DOWN]:
-            kk_rct.move_ip(0, +1)
-        if key_lst[pg.K_LEFT]:
-            kk_rct.move_ip(-1, 0)
-        if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip(+1, 0)
+            #kk_rct.move_ip(0, -1)
+            q=0
+            p=-1
+        elif key_lst[pg.K_DOWN]:
+            #kk_rct.move_ip(0, +1)
+            q=0
+            p=+1
+        elif key_lst[pg.K_LEFT]:
+            #kk_rct.move_ip(-1, 0)
+            q=-1
+            p=0
+        elif key_lst[pg.K_RIGHT]:
+            #kk_rct.move_ip(+1, 0)
+            q=+1
+            p=0
         else:
             kk_rct.move_ip(-1, 0)
-
+        kk_rct.move_ip(q, p)
         x=tmr%3200
         screen.blit(bg_img, [-x, 0])#6
-        screen.blit(kk_img, kk_rct)#4,10
         screen.blit(bg_img2, [-x+1600, 0])#
         screen.blit(bg_img, [-x+3200, 0])#
+        screen.blit(kk_img, kk_rct)#4,10
         pg.display.update()
         tmr += 1        
         clock.tick(200)
-
-
+        
 if __name__ == "__main__":
     pg.init()
     main()
